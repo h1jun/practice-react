5753638023a267557b7f4f33827deae0ae3ae235
h1jun <dittoavec@gmail.com>
2021-10-20 23:59:32 +0900

    Add wordrelay game

---
+++ 02_word-chain-game/WordRelay.jsx
@@ -0,0 +1,15 @@
+// 필요로 하는 라이브러리나 패키지 등을 가져오는 부분
+const React = require('react')
+const { Component } = React;
+
+class WordRelay extends Component {
+    state = {
+
+    }
+    render () {
+
+    }
+}
+
+// 내가 쪼갠 컴포넌트를 밖에서 사용할 수 있도록 해주는
+module.exports = WordRelay;
---
+++ 02_word-chain-game/client.jsx
@@ -0,0 +1,5 @@
+const { Component } = require('react');
+const React = require('react');
+const ReactDom = require('react-dom');
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
